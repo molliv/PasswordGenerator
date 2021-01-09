2ed0e4aca786f85593cda1b0175483a547d66f16
Molli V <mollitalkin@hotmail.com>
2021-01-09 09:33:06 -0800

    password functions

--- script.js
+++ script.js
@@ -11,12 +11,35 @@ var spcialCharacters = [
 //array of uppercase letters
 var upperCasedCharacters = [
   "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-]
+];
 
 //array of lowercase letters
 var lowerCasedCharacters = [
   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-]
+];
+
+//function to prompt user for password options
+function getPasswordOptions() {
+  // defining length variable and conditional statements
+  var length = parseInt(prompt("How long do you want your password?"));
+
+  if (isNaN(length) === true) {
+    alert("Length of password must be provided as a numerica number");
+    return;
+  }
+
+  if (length < 8) {
+    alert("Password must be at least 8 characters");
+    return;
+  }
+
+  if (length > 128) {
+    alert("Password can be no longer than 128 characters");
+    return;
+  }
+
+  
+}
 
 // Assignment Code 
 var generateBtn = document.querySelector("#generate");
